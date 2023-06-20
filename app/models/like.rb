f139b688ed157eb7e66ea7ffcd11e2fe87d363de
Odongo006 <www.henryodongo006@gmail.com>
2023-06-20 15:33:18 +0300

    Use models to insert data

---
+++ app/models/like.rb
@@ -0,0 +1,14 @@
+class Like < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    belongs_to :post
+  
+    after_save :update_post_likes_counter
+    after_destroy :update_post_likes_counter
+  
+    private
+  
+    def update_post_likes_counter
+      post.update(likes_counter: post.likes.count)
+    end
+  end
+  
