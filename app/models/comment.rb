4bbebc2e7ec8e6d95d1206126dceb15c4d6324c1
Odongo006 <www.henryodongo006@gmail.com>
2023-06-30 23:39:16 +0300

    Complete the project

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
   after_save :update_post_comments_counter
@@ -12,5 +11,4 @@ class Comment < ApplicationRecord
   end
 
   validates :text, presence: true
-  validates :user_id, presence: true
 end
