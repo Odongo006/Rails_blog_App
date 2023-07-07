3319be8a11f910f497ed9e7bc7f53fa616af551c
Odongo006 <www.henryodongo006@gmail.com>
2023-07-07 14:37:34 +0300

    Fix errors and failures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :photo, :bio, :email, :password)
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation, :current_password)
     end
   end
 end
