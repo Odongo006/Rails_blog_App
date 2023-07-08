121922c6fb9ee8590ab1201ec735aaad24e1d5ed
Odongo006 <www.henryodongo006@gmail.com>
2023-07-08 21:23:31 +0300

    Fix Changes requested by the reviewer

--- Gemfile
+++ Gemfile
@@ -75,4 +75,4 @@ end
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
-gem "devise", "~> 4.9"
+gem 'devise', '~> 4.9'
--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,38 +1,20 @@
 require 'rails_helper'
+require 'capybara/rspec'
+require 'capybara/rails'
 
 RSpec.describe 'Users', type: :feature do
   before do
-    @user1 = User.create!(name: 'Tom', posts_counter: 0)
-    @user2 = User.create!(name: 'Toro', posts_counter: 0, bio: 'developer')
-    @user3 = User.create!(name: 'Ted', posts_counter: 0)
+    @user1 = User.create!(name: 'Tom', photo: 'www.unsplash.com', posts_counter: 0)
+    @user2 = User.create!(name: 'Toro', photo: 'www.unsplash.com', posts_counter: 0, bio: 'developer')
+    @user3 = User.create!(name: 'Ted', photo: 'www.unsplash.com', posts_counter: 0)
 
     visit users_path
   end
 
-  describe 'index page' do
-    it 'displays usernames of all other users' do
-      expect(page).to have_content('Tom')
-      expect(page).to have_content('Toro')
-      expect(page).to have_content('Ted')
-    end
-
-    it 'displays profile picture for each user' do
-      expect(page).to have_css('div.user-photo', count: 5)
-    end
-
-    it 'displays the number of posts each user has written' do
-      expect(page).to have_content('Number of posts: 0', count: 3) # Assuming each user has 5 posts
-    end
-
-    it 'redirects to user show page when clicking on a user' do
-      click_link 'Toro'
-      expect(page).to have_current_path(user_path(@user2))
-    end
-  end
-
   describe 'show page' do
     before do
-      @post = Post.create!(title: 'Tests', text: 'Test are good', author: @user2, comments_counter: 0, likes_counter: 0)
+      @post = Post.create!(title: 'Tests', text: 'Test are good', author: @user2, comments_counter: 0,
+                           likes_counter: 0)
       @comment = Comment.create!(text: 'Nice post', author: @user2, post: @post)
       visit user_path(@user2)
     end
